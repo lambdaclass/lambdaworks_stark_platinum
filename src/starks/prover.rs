631e6f2cba99b134072fd0b1d6430b3434e5e20b
gabrielbosio <gabrielbosio95@gmail.com>
2023-06-23 17:48:43 -0300

    Print only the last elem of evals

--- src/starks/prover.rs
+++ src/starks/prover.rs
@@ -111,7 +111,7 @@ where
     // Evaluate those polynomials t_j on the large domain D_LDE.
     let evaluations = p.evaluate_offset_fft(blowup_factor, Some(domain_size), offset)?;
     let step = evaluations.len() / (domain_size * blowup_factor);
-    dbg!(&evaluations);
+    dbg!(&evaluations.last().unwrap());
     dbg!(step);
     match step {
         1 => Ok(evaluations),
