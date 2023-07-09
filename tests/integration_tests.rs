8a686844fb6e96fb0d9d4eb7f4a3374c9d232d26
MauroFab <maurotoscano2@gmail.com>
2023-07-09 18:01:46 -0300

    wip

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,9 +1,9 @@
 use std::ops::Range;
 
-use lambdaworks_math::field::fields::{
+use lambdaworks_math::{field::fields::{
     fft_friendly::stark_252_prime_field::Stark252PrimeField as F,
     u64_prime_field::{F17, FE17},
-};
+}, traits::{Serializable, Deserializable}};
 use lambdaworks_stark::{
     cairo::{
         air::{
@@ -25,7 +25,7 @@ use lambdaworks_stark::{
             quadratic_air::{self, QuadraticAIR, QuadraticPublicInputs},
             simple_fibonacci::{self, FibonacciAIR, FibonacciPublicInputs},
         },
-        proof::options::ProofOptions,
+        proof::{options::ProofOptions, stark::StarkProof},
         prover::prove,
         trace::TraceTable,
         verifier::verify,
@@ -112,6 +112,7 @@ fn test_prove_quadratic() {
 }
 
 /// Loads the program in path, runs it with the Cairo VM, and makes a proof of it
+
 fn test_prove_cairo_program(file_path: &str, output_range: &Option<Range<u64>>) {
     let proof_options = ProofOptions::default_test_options();
 
@@ -124,6 +125,7 @@ fn test_prove_cairo_program(file_path: &str, output_range: &Option<Range<u64>>) 
 }
 
 /// Loads the program in path, runs it with the Cairo VM, and makes a proof of it
+
 fn test_prove_cairo1_program(file_path: &str) {
     let proof_options = ProofOptions::default_test_options();
     let program_content = std::fs::read(file_path).unwrap();
@@ -135,38 +137,45 @@ fn test_prove_cairo1_program(file_path: &str) {
 }
 
 #[test_log::test]
+
 fn test_prove_cairo_simple_program() {
     test_prove_cairo_program(&cairo0_program_path("simple_program.json"), &None);
 }
 
 #[test_log::test]
+
 fn test_prove_cairo_fibonacci_5() {
     test_prove_cairo_program(&cairo0_program_path("fibonacci_5.json"), &None);
 }
 
 #[cfg_attr(feature = "metal", ignore)]
+
 #[test_log::test]
 fn test_prove_cairo_fibonacci_casm() {
     test_prove_cairo1_program(&cairo1_program_path("fibonacci_cairo1.casm"));
 }
 
 #[test_log::test]
+
 fn test_prove_cairo_rc_program() {
     test_prove_cairo_program(&cairo0_program_path("rc_program.json"), &None);
 }
 
 #[test_log::test]
+
 fn test_prove_cairo_lt_comparison() {
     test_prove_cairo_program(&cairo0_program_path("lt_comparison.json"), &None);
 }
 
 #[cfg_attr(feature = "metal", ignore)]
+
 #[test_log::test]
 fn test_prove_cairo_compare_lesser_array() {
     test_prove_cairo_program(&cairo0_program_path("compare_lesser_array.json"), &None);
 }
 
 #[test_log::test]
+
 fn test_prove_cairo_output_and_rc_program() {
     test_prove_cairo_program(&cairo0_program_path("signed_div_rem.json"), &Some(289..293));
 }
@@ -204,6 +213,7 @@ fn test_prove_dummy() {
 }
 
 #[test_log::test]
+
 fn test_verifier_rejects_proof_of_a_slightly_different_program() {
     let program_content = std::fs::read(cairo0_program_path("simple_program.json")).unwrap();
     let (main_trace, mut pub_input) =
@@ -224,6 +234,7 @@ fn test_verifier_rejects_proof_of_a_slightly_different_program() {
 }
 
 #[test_log::test]
+
 fn test_verifier_rejects_proof_with_different_range_bounds() {
     let program_content = std::fs::read(cairo0_program_path("simple_program.json")).unwrap();
     let (main_trace, mut pub_inputs) =
@@ -241,6 +252,7 @@ fn test_verifier_rejects_proof_with_different_range_bounds() {
 }
 
 #[test_log::test]
+
 fn test_verifier_rejects_proof_with_changed_range_check_value() {
     // In this test we change the range-check value in the trace, so the constraint
     // that asserts that the sum of the rc decomposed values is equal to the
@@ -266,6 +278,7 @@ fn test_verifier_rejects_proof_with_changed_range_check_value() {
 }
 
 #[test_log::test]
+
 fn test_verifier_rejects_proof_with_overflowing_range_check_value() {
     // In this test we manually insert a value greater than 2^128 in the range-check builtin segment.
 
@@ -302,6 +315,8 @@ fn test_verifier_rejects_proof_with_overflowing_range_check_value() {
 }
 
 #[test_log::test]
+
+
 fn test_verifier_rejects_proof_with_changed_output() {
     let program_content = std::fs::read(cairo0_program_path("output_program.json")).unwrap();
     let (main_trace, pub_inputs) =
